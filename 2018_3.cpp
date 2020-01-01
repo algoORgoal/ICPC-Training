74f4183a8a68ebc877b173c6928552255133dcb9
white <startoflateness@gmail.com>
2020-01-01 20:05:52 +0900

    complelete using DFS

--- 2018_3.cpp
+++ 2018_3.cpp
@@ -1,28 +1,20 @@
 /*
-#2019/12/31 우이산
-#2018년 초등부 3번 문제 (메모리 초과)
+#2019/1/1 우이산
+#2018년 초등부 3번 문제
 #작성언어 c++
 */
 
 #include <cstdio>
+#include <utility>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-typedef struct path {
-	int dest;
-	int weight;
-}Path;
-
 typedef struct node {
-	vector<Path> v;
+	int nodeNum;
+	vector<pair<int, int>> v;
 }Node;
-
-// return an int array that store distance to others
-int*& searchMinPath(Node *& arr, const int& N, const int & pivot/*== starting point*/);
-
-// Return distance between n1 and n2
-int howFar(Node &n1, const int& n2);
 
 int main() {
 	int N,
@@ -37,6 +29,10 @@ int main() {
 	// make an arr for node's info
 	Node* nodeInfo = new Node[N];
 
+	// init node
+	for (int i = 0; i < N; i++)
+		nodeInfo[i].nodeNum = i;
+
 	// get path info
 	for (int i = 0; i < N - 1; i++) {
 		int a, /* == a room linked by the path*/
@@ -45,66 +41,54 @@ int main() {
 
 		scanf("%d %d %d", &a, &b, &weight);
 		// the index started on 0
-		nodeInfo[a - 1].v.push_back({ b - 1, weight });
-		nodeInfo[b - 1].v.push_back({ a - 1, weight });
+		nodeInfo[a - 1].v.push_back(make_pair(b - 1, weight));
+		nodeInfo[b - 1].v.push_back(make_pair(a - 1, weight));
 	}
 
-	// get min path
-	int * arrA = searchMinPath(nodeInfo, N, A);
-	int * arrB = searchMinPath(nodeInfo, N, B);
-
-	// calculate result
-	int result = arrA[0] + arrB[0];
-	for (int i = 0; i < N; i++) {
-		for (int j = i; j < N; j++) {
-			// if there is a path link i to j
-			if (howFar(nodeInfo[i], j) != -1)
-				result = (result < (arrA[i] + arrB[j]))? result : (arrA[i] + arrB[j]);
+	// finde path using DFS
+	stack<Node> s;
+	s.push(nodeInfo[A]);
+	while (s.top().nodeNum != B) {
+		// 현재 노드에서 추가적으로 탐색할 경로가 없다면
+		// 즉, 현재 노드를 경유하는 A에서 B로 이동하는 경로가 존재하지 않는다면
+		if (s.top().v.empty()) {
+			s.pop();
+			s.top().v.pop_back();
+			continue;
 		}
+
+		// 현재 노드에서 추가적윽로 탐색할 경로가 있다면
+		// 즉, 현재 노드를 경유하는 A에서 B로 이동하는 경로가 존재할 가능성이 있다면
+		// 다음 노드를 탐색
+		int nextNode = s.top().v.back().first;
+
+		// 우선, 길을 되돌아가는것을 방지
+		// 다음 노드와 연결된 길 목록에서 현재 노드와 이어지는 길을 제거
+		for (vector<pair<int, int>>::iterator it = nodeInfo[nextNode].v.begin(); it != nodeInfo[nextNode].v.end(); it++) {
+			if ((*it).first == s.top().nodeNum) {
+				nodeInfo[nextNode].v.erase(it);
+				break;
+			}
+		}
+
+		// 다음 노드를 경로에 추가
+		s.push(nodeInfo[nextNode]);
 	}
 
-	printf("%d\n", result);
+	// calcualte result
+	unsigned long long result = 0;
+	int longest = 0;
+	s.pop();
+	while (!s.empty()) {
+		int distance = s.top().v.back().second;
+		s.pop();
+
+		longest = (longest > distance) ? longest : distance;
+		result += distance;
+	}
+
+	result -= longest;
+	printf("%llu", result);
 
 	return 0;
 }
-
-int howFar(Node & n1, const int& n2) {
-	for (int i = 0; i < n1.v.size(); i++) {
-		if (n1.v.at(i).dest == n2)
-			return n1.v.at(i).weight;
-	}
-	return -1;
-};
-
-int*& searchMinPath(Node*& arr, const int& N, const int& pivot/*== starting point*/) {
-	// init result array
-	int* result = new int[N];
-	for (int i = 0; i < N; i++) {
-		result[i] = -1;
-	}
-	result[pivot] = 0;
-
-	int count = 1;
-	while (count < N) {
-		for (int i = 0; i < N; i++) {
-			// i까지의 경로를 찾지 못했다면
-			if (result[i] == -1)
-				continue;
-
-			for (int j = 0; j < N; j++) {
-				// j까지의 경로를 이미 알고 있다면
-				if (result[j] != -1)
-					continue;
-
-				// i와 j사이를 연결하는 통로가 있다면
-				int distance = howFar(arr[i], j);
-				if (distance != -1) {
-					result[j] = result[i] + distance;
-					count++;
-				}
-			}
-		}
-	}
-
-	return result;
-};
