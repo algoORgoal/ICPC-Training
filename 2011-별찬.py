d5c5b087ed95462d17b8b8d9aeb3855ffd024ccb
algoORgoal <qufstar46@gmail.com>
2020-01-16 02:13:29 +0900

    2011 문제 제출(정답)

--- 2011-별찬.py
+++ 2011-별찬.py
@@ -1,5 +1,11 @@
 
 
+'''
+    날짜: 2020-01-16
+    언어: python
+    문제: 백준 2011번
+    작성자: 김별찬
+'''
 
 '''
 앞의 두자리수가 26 이하인 경우
@@ -17,73 +23,35 @@ f(25114) = f(5114) + f(114)
          = f(14) + f(4) + f(14) + f(4)
 '''
 
-'''
-def crypto(num):
-    if num == 0:
-        return 0
-    elif num <= 10:
-        return 1
-    elif num <= 26:
-        return 2
-    else:
-        i = 1
-        while(num//i >= 10):
-            i *= 10
-        remainder = num % i
-        
-        if(num//(i/10) <= 26):
-            return crypto(remainder) + crypto(remainder%(i/10))
-        else:
-            return crypto(remainder)
-
-num = int(input())
-result = crypto(num)
-print(result%1000000)
-'''
 
 
 
+import sys
+sys.setrecursionlimit(100000)
 def crypto(num, dic, idx_list):
-    print("=======")
-    print(num)
-    if num == '0':
+    if num == '0' or '00' in num or num[0] == '0':
         return 0
-    if num in idx_list:
+    elif num in idx_list:
         if num in idx_list[0:10]:
             return 1
         else:
             return 2
     else:
-        print(num[1:])
-        print(num[2:])
+        
         if num in dic.keys():
             return dic[num]
         else:
             two_d = num[0:2]
-            
             if two_d in idx_list:
-                
                 dic[num] = crypto(num[1:], dic, idx_list) + crypto(num[2:], dic,  idx_list)
             else:
                 dic[num] = crypto(num[1:], dic, idx_list)
-            print("=======")
             return dic[num]
-    
-
-            
-
-
-
-    
-
 str1 = input()
 dic = dict()
 idx_list = list()
 for i in range(1, 27):
     idx_list.append(str(i))
 
-print(idx_list[0:10])
 result = crypto(str1, dic, idx_list)
-print(dic)
-print(result)
 print(result%1000000)
