3515f95fe724f6c1e16a64bd96c64c69fbf98740
algoORgoal <qufstar46@gmail.com>
2020-02-28 22:58:19 +0900

    solved num_2293_별찬.py

---
+++ Baekjoon/num_2293_별찬.py
@@ -0,0 +1,35 @@
+'''
+날짜: 2020-02-28
+상태: 완성(정답)
+'''
+
+
+n, k = input().split()
+n = int(n)
+k = int(k)
+arr = []
+answer = []
+for i in range(n):
+    arr.append(int(input()))
+
+
+for i in range(k + 1):
+    answer.append(0)
+
+# print(answer)
+
+
+for i in range(len(answer)):
+    # print(i)
+    unit = i % arr[0]
+    if (unit == 0):
+        answer[i] += 1
+# print(answer)
+
+
+for i in range(1, n):
+    # print(arr[i])
+    for j in range(arr[i], len(answer)):
+        answer[j] = answer[j] + answer[j - arr[i]]
+    # print(answer)
+print(answer[len(answer) - 1])
